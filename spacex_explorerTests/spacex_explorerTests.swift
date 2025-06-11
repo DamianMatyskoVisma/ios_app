e4f77bd2f7596fa7342271bdd5af814291becedf
Firefly <44482004+DamianMatysko@users.noreply.github.com>
2025-06-11 10:30:21 +0200

    Initial Commit

---
+++ spacex_explorerTests/spacex_explorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  spacex_explorerTests.swift
+//  spacex_explorerTests
+//
+//  Created by damian.matysko on 11/06/2025.
+//
+
+import Testing
+
+struct spacex_explorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ spacex_explorerUITests/spacex_explorerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  spacex_explorerUITestsLaunchTests.swift
+//  spacex_explorerUITests
+//
+//  Created by damian.matysko on 11/06/2025.
+//
+
+import XCTest
+
+final class spacex_explorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
